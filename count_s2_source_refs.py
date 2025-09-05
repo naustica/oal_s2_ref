acef1ff3339f57de62ffcc9dd73a3464d7f1e4ef
Nick Haupka <nick.haupka@outlook.de>
2025-09-05 13:10:33 +0200

    add hpc script

---
+++ count_s2_source_refs.py
@@ -0,0 +1,54 @@
+import json
+from collections import Counter
+from multiprocessing import Pool, cpu_count
+from operator import add
+from functools import reduce
+import glob
+import pickle
+import uuid
+
+
+def create_counter(input_file_path: str, path_to_counter: str) -> None:
+    print(input_file_path)
+    with open(f'{path_to_counter}/counter_{uuid.uuid4()}.pkl', 'wb') as outp:
+        with open(input_file_path, 'r') as file:
+            new_data = []
+            
+            for line in file:
+                new_item = json.loads(line)
+                if new_item.get('citedcorpusid'):
+                    new_data.append(new_item.get('citingcorpusid'))
+    
+            pickle.dump(Counter(new_data), outp, pickle.HIGHEST_PROTOCOL)
+
+def read_pickle_counter(input_file_path: str):
+    with open(input_file_path, 'rb') as file:
+        return pickle.load(file)
+
+def create_counter_in_parallel(path_to_references: str,
+                               path_to_counter: str,
+                               max_workers: int = cpu_count()):
+
+    list_with_files = glob.glob(f'{path_to_references}/*.jsonl')
+
+    with Pool(processes=max_workers) as pool:
+        pool.starmap(create_counter, zip(list_with_files, [path_to_counter] * len(list_with_files)))
+
+def reduce_counter(path_to_counters: str, output_file_path: str) -> None:
+
+    list_with_counters = glob.glob(f'{path_to_counters}/*.pkl')
+
+    c_reduced = reduce(add, (Counter(dict(read_pickle_counter(i))) for i in list_with_counters))
+
+    with open(file=f'{output_file_path}/counter.json', mode='w') as output_file:
+        result = json.dumps(c_reduced)
+        output_file.write(result)
+
+
+if __name__ == '__main__':
+    create_counter_in_parallel(path_to_references='/Users/naustica/Desktop/oal_s2_ref/semantic_scholar_citations_sample_20250715',
+                               path_to_counter='/Users/naustica/Desktop/oal_s2_ref/')
+    reduce_counter(path_to_counters='/Users/naustica/Desktop/oal_s2_ref/',
+                   output_file_path='/Users/naustica/Desktop/oal_s2_ref/')
+
+    
---
+++ s2_source_ref_hpc.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -p medium
+#SBATCH -C scratch
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -c 16
+#SBATCH --mem=300G
+#SBATCH -t 08:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=nick.haupka@sub.uni-goettingen.de
+
+module load python
+
+python3 count_s2_source_refs.py
