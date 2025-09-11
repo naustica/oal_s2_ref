88c71168a1b3c9a9e65d190e45cf04a33edbee8b
Nick Haupka <nick.haupka@outlook.de>
2025-09-11 08:50:58 +0200

    update -t flag

--- s2_source_ref_hpc.sh
+++ s2_source_ref_hpc.sh
@@ -4,8 +4,7 @@
 #SBATCH -N 1
 #SBATCH -n 1
 #SBATCH -c 16
-#SBATCH --mem=300G
-#SBATCH -t 08:00:00
+#SBATCH -t 07:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=nick.haupka@sub.uni-goettingen.de
 
